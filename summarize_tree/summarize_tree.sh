b52bd2d81f33f6d3f582da83dadab6f52ed43344
hoff0899 <hoff0899@morris.umn.edu>
2017-10-18 17:22:07 -0500

    Add clarity and comments to summarize_tree.sh

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/bash
 
+# Save working directory
 home="$(pwd)"
+
+# Get first argument passed as the directory to be entered
 path=$1
 
 cd $path
 
+#Find all items with type directory
 totaldir="$(find . -type d | xargs | wc -w)"
+
+#Find all items with type file
 totalfile="$(find . -type f | xargs | wc -w)"
 
+#Output
 echo "There were "$totaldir" directories."
 echo "There were "$totalfile" regular files."
 
+#return to original directory
 cd $home
